de937f11829e0e27d74e29bc6962b61dc7d742b0
Mukhiddin06 <muhiddinbegiboyev@gmail.com>
2024-01-31 16:52:04 +0500

    initial commit

---
+++ js/main.js
@@ -0,0 +1,98 @@
+// misol-1
+// let arr = [1, 2, 3, 4, 5]
+
+// for(let i = 0; i < arr.length; i++){
+//     if(arr[i] > 0){
+//         console.log(2**arr[i])
+//     }
+// }
+
+
+// misol-3
+// let array = [1, 2, 6, 5, 8, 4, 7, 8, 3, 0, 2]
+
+// array.reverse();
+// console.log(array)
+
+
+
+// misol-4
+// let array = [1, 2, 5, 7, 8, 5, 1, 0, 3, 6, 4]
+// let n = 0
+// for(let i = 1; i < array.length; i+=2){
+//     console.log(array[i])
+// }
+
+
+
+// misol-5
+// let array = [1, 2, 5, 7, 8, 5, 1, 0, 3, 6, 4]
+
+// for(let i = 0; i < array.length; i++){
+//     if(i % 2 !== 0){
+//          console.log(array[i])
+//     }
+// }
+// for(let i = 0; i < array.length; i++){
+//     if(i % 2 == 0){
+//         array.reverse();
+//          console.log(array[i])
+//     }
+// }
+
+
+
+// misol-6
+// let array = [1, 2, 5, 7, 8, 5, 1, 0, 3, 6, 4]
+// for(let i = 0; i < array.length; i+=2){
+//     console.log(array[i])
+// }
+
+
+
+// misol-7
+// let array = [1, 2, 5, 7, 8, 5, 1, 0, 3, 6, 4]
+// array.reverse();
+// for(let i = 0; i < array.length; i+=2){
+//     console.log(array[i])
+// }
+
+
+
+// misol-8
+// let array = [1, 2, 5, 7, 8, 5, 1, 0, 3, 6, 4]
+// for(let i = 0; i < array.length; i+=2){
+//     console.log(array[i])
+// }
+// for(let i = 1; i < array.length; i+=2){
+//     console.log(array[i])
+// }
+
+
+
+// misol-9
+// let array = [1, 2, 5, 7, 8, 5, 1, 0, 3, 6, 4]
+// for(let i = 0; i < array.length; i+=2){
+//     console.log(array[i])
+// }
+// array.reverse();
+// for(let i = 1; i < array.length; i+=2){
+//     console.log(array[i])
+// }
+
+
+
+// misol-21
+// let array = [1, 2, 5, 7, 8, 5, 1, 0, 3, 6, 4]
+// let k = 10;
+// for(let i = 0; i < array.length; i++){
+//     console.log(array[i] + k)
+// }
+
+
+
+// misol-23
+// let array = [1, 2, 6, 5, 8, 4, 7, 8, 3, 0, 2]
+
+// array.reverse();
+// console.log(array)
